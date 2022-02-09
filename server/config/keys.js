9043ce2f74e7bbe7c0d72d7b3ced5a626df2d277
bhabalomkar421 <bhabalomkar421@gmail.com>
2022-02-09 13:05:30 +0530

    db added

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI : "mongodb+srv://intmain:intmain@11@cluster0.tff5j.mongodb.net/Cluster0?retryWrites=true&w=majority",
+    mongoURI : "mongodb+srv://root:root@123@cluster0.tid6b.mongodb.net/JournifyDatabase?retryWrites=true&w=majority",
     secretOrKey : "secret",
     JWT_ACC_ACTIVATE : "secret",
     CLIENT_URL: "http://localhost:3000",
