80db56c4d524971d6bf9e486f5f344a8cfb5e44b
bhabalomkar421 <bhabalomkar421@gmail.com>
2022-06-24 13:10:07 +0530

    issue page

--- src/components/Admin/Issue.js
+++ src/components/Admin/Issue.js
@@ -1,34 +1,90 @@
 import React from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Card from 'react-bootstrap/Card';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button, Container } from 'react-bootstrap';
 
 class Issue extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            pdfs : [
+                {
+                    pdfName : "1.Case Comment: Federation of Obstetrics and Gynaecological Societies of India (FOGSI) V. Union of India and Ors.   ",
+                    authorName : "Gayathri Jiji",
+                    doi : "www.doi.org/10.5281/zenodo.6011662"
+                },
+                {
+                    pdfName : "1.Case Comment: Federation of Obstetrics and Gynaecological Societies of India (FOGSI) V. Union of India and Ors.   ",
+                    authorName : "Gayathri Jiji",
+                    doi : "www.doi.org/10.5281/zenodo.6011662"
+                },
+                {
+                    pdfName : "1.Case Comment: Federation of Obstetrics and Gynaecological Societies of India (FOGSI) V. Union of India and Ors.   ",
+                    authorName : "Gayathri Jiji",
+                    doi : "www.doi.org/10.5281/zenodo.6011662"
+                },
+                {
+                    pdfName : "1.Case Comment: Federation of Obstetrics and Gynaecological Societies of India (FOGSI) V. Union of India and Ors.   ",
+                    authorName : "Gayathri Jiji",
+                    doi : "www.doi.org/10.5281/zenodo.6011662"
+                },
+                {
+                    pdfName : "1.Case Comment: Federation of Obstetrics and Gynaecological Societies of India (FOGSI) V. Union of India and Ors.   ",
+                    authorName : "Gayathri Jiji",
+                    doi : "www.doi.org/10.5281/zenodo.6011662"
+                }
+            ]
+        }
+    }
+
+    componentDidMount(){
+        // axios.get('/my-documents/')
+        // .then(res => {
+        //     // console.log(res.data);
+        //     this.setState({ myforms : res.data.myforms })
+        //     // console.log(this.state.myforms);
+        // })
+        // .catch(err => console.log(err));
+    }
+
     render(){
         return (
             <div>
-<Row>
-                            <Col>
-                                <Link to='/journal/Federation' >
-                                    <Card className="C_D card card-1" style={{ width: '35rem' }}>
+<Container>
+  
+    
+                            {
 
-                                        <Card.Body>
-                                            <Card.Title className="journytitles">1.Case Comment: Federation of Obstetrics and Gynaecological Societies of India (FOGSI) V. Union of India and Ors.   </Card.Title>
-                                            <Card.Title>
+                            
+                                this.state.pdfs.map((pdf) => (
+                                    <Row> 
+                                    <Col>
+                                        <div>
+                                        <Link to='/journal/Federation' >
+                                            <Card className="card-horizontal">
+
+                                                <Card.Body className="Cardbody">
+                                                    <Card.Title >{pdf.pdfName} </Card.Title>
+                                                    <Card.Title>
+                                                        
+                                                    <Card.Title > Author - {pdf.authorName}  </Card.Title>
+                                                    </Card.Title>
+
+                                                    <Card.Text  className="doi">
+                                                    DOI: {pdf.doi}
+                                                        </Card.Text>                                            
                                                 
-                                               <Card.Title className = "journytitles"> Author - Gayathri Jiji  </Card.Title>
-                                            </Card.Title>
-
-                                            <Card.Text  className="doi">
-                                            DOI: www.doi.org/10.5281/zenodo.6011662
-                                                </Card.Text>                                            
-                                           
-                                        </Card.Body>
-                                      
-                                    </Card>
-                                </Link>  
-                            </Col>
-                            </Row>
+                                                </Card.Body>
+                                            
+                                            </Card>
+                                        </Link>  
+                                        </div>
+                                    </Col>
+                                    </Row> 
+                                ))
+                            
+                            }
+                </Container>
             </div>
         )
     }
