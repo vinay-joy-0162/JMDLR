d9d72fd1344c30e802a23e8c217b28462b316446
vinay-joy-0162 <67511735+vinay-joy-0162@users.noreply.github.com>
2022-06-24 12:43:23 +0530

    latestupdates

--- src/components/Admin/Issue.js
+++ src/components/Admin/Issue.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Card from 'react-bootstrap/Card';
+import { Row, Col, Button } from 'react-bootstrap';
 
 class Issue extends React.Component{
     render(){
         return (
             <div>
-     Issue 1
+<Row>
+                            <Col>
+                                <Link to='/journal/Federation' >
+                                    <Card className="C_D card card-1" style={{ width: '35rem' }}>
+
+                                        <Card.Body>
+                                            <Card.Title className="journytitles">1.Case Comment: Federation of Obstetrics and Gynaecological Societies of India (FOGSI) V. Union of India and Ors.   </Card.Title>
+                                            <Card.Title>
+                                                
+                                               <Card.Title className = "journytitles"> Author - Gayathri Jiji  </Card.Title>
+                                            </Card.Title>
+
+                                            <Card.Text  className="doi">
+                                            DOI: www.doi.org/10.5281/zenodo.6011662
+                                                </Card.Text>                                            
+                                           
+                                        </Card.Body>
+                                      
+                                    </Card>
+                                </Link>  
+                            </Col>
+                            </Row>
             </div>
         )
     }
